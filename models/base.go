d79d5a7af9e34eae9f01540dbba740c9bcd219f0
周泓滨 <zhouhongbin@163.com>
2021-10-14 15:07:23 +0800

    update

--- models/base.go
+++ models/base.go
@@ -9,10 +9,10 @@ var O orm.Ormer
 
 func init() {
 	// set default database
-	orm.RegisterDriver("mysql", orm.DRMySQL)                                                                   //使用mysql数据库
-	orm.RegisterDataBase("default", "mysql", "root:123456@tcp(127.0.0.1:3306)/go_test?charset=utf8&loc=Local") //注册mysql（default为数据库别名）
-	orm.RegisterModel(new(Dbuser))                                                                             //设置model
-	orm.RunSyncdb("default", false, true)                                                                      //根据model创建表
+	orm.RegisterDriver("mysql", orm.DRMySQL)                                                         //使用mysql数据库
+	orm.RegisterDataBase("default", "mysql", "root:123456@tcp(127.0.0.1:3306)/go_test?charset=utf8") //注册mysql（default为数据库别名）
+	orm.RegisterModel(new(Dbuser))                                                                   //设置model
+	orm.RunSyncdb("default", false, true)                                                            //根据model创建表
 	O = orm.NewOrm()
 }
 
