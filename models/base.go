f806a3b5121daa6f131f62a5239c41afb60a93de
周泓滨 <zhouhongbin@163.com>
2021-12-15 16:45:21 +0800

    feat: 数据库配置读取

--- models/base.go
+++ models/base.go
@@ -1,19 +1,27 @@
 package models
 
 import (
+	"fmt"
+
 	"github.com/beego/beego/v2/client/orm"
+	"github.com/beego/beego/v2/core/config"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 var O orm.Ormer
 
 func init() {
-	// set default database
-	orm.RegisterDriver("mysql", orm.DRMySQL)                                                         //使用mysql数据库
-	orm.RegisterDataBase("default", "mysql", "root:123456@tcp(127.0.0.1:3306)/go_test?charset=utf8") //注册mysql（default为数据库别名）
-	orm.RegisterModel(new(Dbuser))                                                                   //设置model
-	orm.RegisterModel(new(List))                                                                     //设置model
-	orm.RunSyncdb("default", false, true)                                                            //根据model创建表
+	mysqladmin, _ := config.String("mysqladmin")
+	mysqlPwd, _ := config.String("mysqlPwd")
+	mysqlDB, _ := config.String("mysqlDB")
+	mysqlUrl, _ := config.String("mysqlUrl")
+	mysqlCharset, _ := config.String("mysqlCharset")
+	fmt.Println(mysqladmin, mysqlPwd, mysqlDB, mysqlUrl, mysqlCharset)
+	orm.RegisterDriver("mysql", orm.DRMySQL)                                                                                                    //使用mysql数据库
+	orm.RegisterDataBase("default", "mysql", fmt.Sprintf("%v:%v@tcp(%v)/%v?charset=%v", mysqladmin, mysqlPwd, mysqlUrl, mysqlDB, mysqlCharset)) //注册mysql（default为数据库别名）
+	orm.RegisterModel(new(Dbuser))                                                                                                              //设置model
+	orm.RegisterModel(new(List))                                                                                                                //设置model
+	orm.RunSyncdb("default", false, true)                                                                                                       //根据model创建表
 	O = orm.NewOrm()
 }
 
